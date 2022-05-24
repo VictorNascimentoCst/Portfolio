f7754d3d99fef59452bac6655392ef4d762faf2a
Victor Nascimento <nascimento-77@hotmail.com>
2022-05-24 19:42:32 -0300

    Add animation to project section

--- path/index.js
+++ path/index.js
@@ -15,3 +15,37 @@
         document.querySelector('.card').style.display = 'flex'
         else document.querySelector('.card').style.display = 'none'
     }   
+
+
+
+
+
+
+
+
+var btn = document.querySelector('button');
+var hint = document.getElementById('hint');
+var height = hint.clientHeight;
+var width = hint.clientWidth;
+var display = hint.style.display 
+console.log(width + 'x' + height);
+// initialize them (within hint.style)
+
+
+function teste(){
+    if(hint.style.visibility == 'hidden'){
+        hint.style.visibility = 'visible';
+        //hint.style.opacity = '1';
+        hint.style.height = '380px';
+        hint.style.width = '100%';
+        hint.style.padding = '15px';
+      }
+  else{
+    hint.style.visibility = 'hidden';
+    //hint.style.opacity = '0';
+    hint.style.height = '0';
+    hint.style.width = '0';
+    hint.style.padding = '0';
+  }
+
+};
